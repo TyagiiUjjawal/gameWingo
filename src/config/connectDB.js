97412748561d9f27cd437c88ed592ddba33034ba
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2024-08-04 19:01:59 +0530

    dsdsdsdsd

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,10 +5,8 @@ const connection = mysql.createPool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    connectTimeout: 90000, // Default to 10 seconds
-    waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    connectTimeout: 990000, // Default to 10 seconds
+
 });
 connection.getConnection()
   .then(conn => {
