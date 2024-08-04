fe3c051092d9bf1dfb47bf741fb51b572a8ecab3
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2024-08-04 16:29:46 +0530

    first commit

---
+++ src/config/connectDB.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+});
+console.log('DATABASE_HOST:', process.env.DATABASE_HOST);
+console.log('DATABASE_USER:', process.env.DATABASE_USER);
+console.log('DATABASE_PASSWORD:', process.env.DATABASE_PASSWORD);
+console.log('DATABASE_NAME:', process.env.DATABASE_NAME);
+
+export default connection;
